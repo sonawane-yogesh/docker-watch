c5e4a325432779d09489830e75ae46dabd06133a
Yogesh Sonawane <y.sonawane@serrala.com>
2023-08-17 11:42:17 +0530

    test-on-other-system

--- src/calculator.js
+++ src/calculator.js
@@ -3,7 +3,9 @@ module.exports = {
         return a + b;
     }, subtract: function (a, b) {
         return a - b;
-    }, devide: function (a, b) {
+    }, divide: function (a, b) {
         return a / b;
+    }, multiply: function (a, b) {
+        return a * b;
     }
 };
