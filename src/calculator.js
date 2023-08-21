9c55c660477d13945a4ef4d89e4e31c5b345a8eb
Yogesh Sonawane <y.sonawane@serrala.com>
2023-08-22 00:06:22 +0530

    argo-check

--- src/calculator.js
+++ src/calculator.js
@@ -11,5 +11,7 @@ module.exports = {
         return a + b;
     }, justMultiply: function (a, b) {
         return a * b;
+    }, argoCheck: function (message) {
+        return "Hello, from ArgoCD " + message;
     }
 };
