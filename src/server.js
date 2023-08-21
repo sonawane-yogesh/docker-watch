d0f76e78af450b83fc6e2926b285733475eac859
Yogesh Sonawane <y.sonawane@serrala.com>
2023-08-22 00:43:17 +0530

    some-modifications

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
 let maths = require("./calculator");
+app.use((request, response, next) => {
+    console.log(`---==== ${request.url} ====---`);
+    next();
+});
 app.get("/", function (request, response, next) {
     response.status(200).json({ message: "App is up and running!" }).end();
 });
@@ -12,6 +16,12 @@ app.get("/subtract", function (request, response) {
     let subtract = maths.subtract(6, 9);
     response.status(200).json({ data: subtract }).end();
 });
+app.get("/message/:msg", function (request, response) {
+    let msg = request.params.msg;
+    let message = maths.argoCheck(msg);
+    response.status(200).json({ data: message }).end();
+});
 app.listen(8080, function () {
     console.log("Server is up and running now on port: 8080");
+    console.log("Started server!");
 });
