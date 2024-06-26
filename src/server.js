b9cb4a85c408b52069191843a8cfc548590a155d
sonawane-yogesh <sonawaneyogesh@hotmail.com>
2024-06-26 19:46:40 +0530

    temp-changes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
-let express = require("express");
-let app = express();
-let maths = require("./calculator");
-let chalk = require("chalk");
-let cors = require("cors");
+const express = require("express");
+const app = express();
+const maths = require("./calculator");
+const chalk = require("chalk");
+const cors = require("cors");
+const http2 = require("http2");
+const dotenv = require("dotenv");
+const { join, resolve } = require("path");
+const { readFileSync } = require("fs");
+dotenv.config();
 
 app.use(cors());
 
 app.use((request, response, next) => {
-    let dt = new Date().toLocaleString("en-us")
+    const dt = new Date().toLocaleString("en-us")
     console.log(`---=== [${chalk.yellow(dt)}] # ${chalk.green(request.url)} ===---\n`);
     next();
 });
@@ -16,37 +21,44 @@ app.get("/", function (request, response, next) {
     response.status(200).json({ message: "App is up and running!!!" }).end();
 });
 app.get("/add/:a/:b", function (request, response) {
-    let a = parseInt(request.params.a);
-    let b = parseInt(request.params.b);
-    let sum = maths.add(a, b);
+    const a = parseInt(request.params.a);
+    const b = parseInt(request.params.b);
+    const sum = maths.add(a, b);
     response.status(200).json({ data: sum }).end();
 });
 app.get("/subtract/:a/:b", function (request, response) {
-    let a = parseInt(request.params.a);
-    let b = parseInt(request.params.b);
-    let subtract = maths.subtract(a, b);
+    const a = parseInt(request.params.a);
+    const b = parseInt(request.params.b);
+    const subtract = maths.subtract(a, b);
     response.status(200).json({ data: subtract }).end();
 });
 app.get("/multiply/:a/:b", function (request, response) {
-    let a = parseInt(request.params.a);
-    let b = parseInt(request.params.b);
-    let subtract = maths.multiply(a, b);
+    const a = parseInt(request.params.a);
+    const b = parseInt(request.params.b);
+    const subtract = maths.multiply(a, b);
     response.status(200).json({ data: subtract }).end();
 });
 app.get("/divide/:a/:b", function (request, response) {
-    let a = parseInt(request.params.a);
-    let b = parseInt(request.params.b);
-    let divide = maths.divide(a, b);
+    const a = parseInt(request.params.a);
+    const b = parseInt(request.params.b);
+    const divide = maths.divide(a, b);
     response.status(200).json({ data: divide }).end();
 });
+const crtPath = resolve(__dirname, "../", 'certificates');
+const http2Server = http2.createSecureServer({
+    cert: readFileSync(join(crtPath, 'device.crt')),
+    key: readFileSync(join(crtPath, 'device.key')),
+    allowHTTP1: true,
+    ALPNProtocols: ["h2"]
 
-let port = 4000;
-app.listen(port, function () {
+}, app);
+const port = process.env.PORT;
+http2Server.listen(port, function () {
     var address = this.address();
     var dt = new Date().toLocaleString("en-us")
     console.log('=======================================================================');
     console.log(chalk.red("Restarted app: " + chalk.green(dt)));
-    console.log(`Kafka server application is up running on port: ${port}`);
+    console.log(`Server application is up running on port: ${port}`);
     console.log(JSON.stringify(address));
     console.log('=======================================================================');
 });
